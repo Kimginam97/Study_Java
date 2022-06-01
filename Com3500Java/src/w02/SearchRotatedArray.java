845c07df1e2726873fd9fd817bf660308fd23a08
Kimginam <rlarlska79@naver.com>
2022-06-02 07:48:05 +0900

    회전된 배열에서 검색 (인덱스 값 찾아오기)

---
+++ Com3500Java/src/w02/SearchRotatedArray.java
@@ -0,0 +1,49 @@
+package w02;
+
+public class SearchRotatedArray {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{20, 25, 26, 29, 33, 1, 3, 5, 6, 10, 11, 19};
+
+        int index = indexOfRotatedArray(numbers, 0, numbers.length - 1, 0);
+
+        System.out.println(index);
+
+        index = indexOfRotatedArray(numbers, 0, numbers.length - 1, 1);
+
+        System.out.println(index);
+
+        index = indexOfRotatedArray(numbers, 0, numbers.length - 1, 29);
+
+        System.out.println(index);
+
+        index = indexOfRotatedArray(numbers, 0, numbers.length - 1, 6);
+
+        System.out.println(index);
+    }
+
+    private static int indexOfRotatedArray(int[] numbers, int start, int end, int num) {
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = (start + end) / 2;
+
+        if (numbers[mid] == num) {
+            return mid;
+        }
+
+        if (numbers[start] <= numbers[mid]) {
+            if (num >= numbers[start] && num <= numbers[mid]) {
+                return indexOfRotatedArray(numbers, start, mid - 1, num);
+            }
+
+            return indexOfRotatedArray(numbers, mid + 1, end, num);
+        }
+
+        if (num >= numbers[mid] && num <= numbers[end]) {
+            return indexOfRotatedArray(numbers, mid + 1, end, num);
+        }
+
+        return indexOfRotatedArray(numbers, start, mid - 1, num);
+    }
+}

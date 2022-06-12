4f7750c7956f03a20ea8328a1d228f29106752b8
Kimginam97 <rlarlska79@naver.com>
2022-06-13 06:29:36 +0900

    Lose Lose 해시함수

---
+++ Com3500Java/src/w04/HashFunction.java
@@ -0,0 +1,23 @@
+package w04;
+
+public class HashFunction {
+
+    /*
+    * LoseLose 해시 함수
+    * 프로그래밍 책에서 간단히 소개하려고 만든코드
+    * 대부분의 해시함수는 부호없는 정수형 사용
+    * java 만 부호없는 정수형이 없다
+    * java 구현할시 쓸데없는짓을 함
+    * 매우간단하지만 충돌이 많음
+    *
+    * byte -> 아스키코드 -> 0~255
+    * */
+
+//    public uint LoseLose(byte[] str){
+//        uint hash = 0;
+//        foreach (byte c in str){
+//            hash += c;
+//        }
+//        return hash;
+//    }
+}

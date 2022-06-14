4325f775489b84075a7ef4110debc7ef359a4d54
Kimginam97 <rlarlska79@naver.com>
2022-06-15 07:13:28 +0900

    비밀번호 해쉬 만들기

---
+++ Com3500Java/src/w04/PassWordHashing.java
@@ -0,0 +1,28 @@
+package w04;
+
+public class PassWordHashing {
+    public static void main(String[] args) {
+        User user = new User("myemail@email.com",
+                "My StRoNg P@$$word");
+
+        System.out.println(
+                String.format("ID: %s",
+                        user.getId()));
+
+        System.out.println(
+                String.format("Email: %s",
+                        user.getEmail()));
+
+        System.out.println(
+                String.format("Password Hash: %s",
+                        user.getPaswordHash()));
+
+        boolean isValid = user.isValidPassword("wrong password!");
+
+        System.out.println(isValid);
+
+        isValid = user.isValidPassword("My StRoNg P@$$word");
+
+        System.out.println(isValid);
+    }
+}

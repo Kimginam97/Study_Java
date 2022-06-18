b49df0312af69a0356c61358dc4278a1330b0b42
Kimginam97 <rlarlska79@naver.com>
2022-06-19 07:47:38 +0900

    BST 삽입

---
+++ Com3500Java/src/w06/Node.java
@@ -0,0 +1,38 @@
+package w06;
+
+public class Node {
+    private final int data;
+    private Node left;
+    private Node right;
+
+    public Node(final int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+
+    public Node getLeft() {
+        return this.left;
+    }
+
+    public Node getRight() {
+        return this.right;
+    }
+
+    public static Node insertRecursive(final Node node, int data) {
+        if (node == null) {
+            return new Node(data);
+        }
+
+        if (data < node.data) {
+            node.left = insertRecursive(node.left,
+                    data);
+        } else {
+            node.right = insertRecursive(node.right, data);
+        }
+
+        return node;
+    }
+}
---
+++ Com3500Java/src/w06/Program.java
@@ -0,0 +1,65 @@
+package w06;
+
+public class Program {
+    public static void main(String[] args) {
+        Node root = new Node(50);
+
+        Node.insertRecursive(root, 24);
+        Node.insertRecursive(root, 42);
+        Node.insertRecursive(root, 33);
+        Node.insertRecursive(root, 22);
+
+        Node.insertRecursive(root, 55);
+        Node.insertRecursive(root, 52);
+        Node.insertRecursive(root, 57);
+
+        int num = root.getData();
+        System.out.println(num);
+
+        num = root.getLeft().getData();
+        System.out.print(num);
+
+        System.out.print(" ");
+
+        num = root.getRight().getData();
+        System.out.print(num);
+
+        System.out.println();
+
+        num = root.getLeft().getLeft()
+                .getData();
+        System.out.print(num);
+
+        System.out.print(" ");
+
+        num = root.getLeft().getRight()
+                .getData();
+        System.out.print(num);
+
+        System.out.print(" ");
+
+        num = root.getRight().getLeft()
+                .getData();
+        System.out.print(num);
+
+        System.out.print(" ");
+
+        num = root.getRight().getRight()
+                .getData();
+        System.out.print(num);
+
+        System.out.println();
+
+        num = root.getLeft().getRight()
+                .getLeft().getData();
+        System.out.print(num);
+
+        System.out.println();
+
+        Node.insertRecursive(root, 53);
+
+        num = root.getRight().getLeft()
+                .getRight().getData();
+        System.out.print(num);
+    }
+}
